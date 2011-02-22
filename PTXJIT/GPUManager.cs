1fcec414bb074569717985b0e6155fafdedc3ef7
morten <morten@2fc84f4a-1799-4a5e-895e-b8a002bf22c3>
2011-02-22 15:06:04 +0000

    Added PTXJIT
    git-svn-id: file:///svn/Ac3IL@26 2fc84f4a-1799-4a5e-895e-b8a002bf22c3

---
+++ PTXJIT/GPUManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Mono.Cecil;
+
+namespace PTXJIT
+{
+    public class GPUManager : AccCIL.AccelleratorBase
+    {
+
+        protected override T DoAccelerate<T>(string assembly, string methodName, params object[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void LoadProgram(IEnumerable<AccCIL.ICompiledMethod> methods)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object Execute(params object[] arguments)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
