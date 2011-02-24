c5e676445b5dabb0473fb6662e5cc7e0f9b67914
morten <morten@2fc84f4a-1799-4a5e-895e-b8a002bf22c3>
2011-02-24 10:06:28 +0000

    Rename PXTJIT to GPUJIT
    git-svn-id: file:///svn/Ac3IL@31 2fc84f4a-1799-4a5e-895e-b8a002bf22c3

---
+++ GPUJIT/GPUJITCompiler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AccCIL;
+
+namespace PTXJIT
+{
+    class GPUJITCompiler : IJITCompiler
+    {
+        /// <summary>
+        /// The list of all mappped CIL to SPE translations
+        /// </summary>
+        private static readonly Dictionary<Mono.Cecil.Cil.Code, System.Reflection.MethodInfo> _opTranslations;
+
+        /// <summary>
+        /// Static initializer for building instruction table based on reflection
+        /// </summary>
+        static GPUJITCompiler()
+        {
+            _opTranslations = BuildTranslationTable();
+        }
+
+        private static Dictionary<Mono.Cecil.Cil.Code, System.Reflection.MethodInfo> BuildTranslationTable()
+        {
+            Dictionary<Mono.Cecil.Cil.Code, System.Reflection.MethodInfo> res = new Dictionary<Mono.Cecil.Cil.Code, System.Reflection.MethodInfo>();
+
+            Mono.Cecil.Cil.Code v;
+            foreach (System.Reflection.MethodInfo mi in typeof(GPUOpCodeMapper).GetMethods(System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.Public))
+                if (Enum.TryParse<Mono.Cecil.Cil.Code>(mi.Name, true, out v))
+                    res[v] = mi;
+
+            return res;
+        }
+
+
+        public ICompiledMethod JIT(AccCIL.IR.MethodEntry method)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
